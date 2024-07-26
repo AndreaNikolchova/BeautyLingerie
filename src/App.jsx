5466ca25ef8fc7e81aa1bc857947dfb414343870
AndreaNikolchova <a.nikolchova04@gmail.com>
2024-07-26 14:01:35 +0300

    Made navbar and started working on list of products

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './componets/Header';
+import Products from './componets/Products';
+function App() {
+ 
+  return (
+    <>
+    
+    <Header/>
+    <Products/>
+      </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: { 
+      colors: {
+      'beige': '#FBF4F2',
+    },},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
