3f9a2de05cb4673b9752220dbd12d9da306b33ef
AndreaNikolchova <a.nikolchova04@gmail.com>
2024-07-26 18:10:46 +0300

    Added new home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
+import {Routes, Route} from 'react-router-dom';
 import Header from './componets/Header';
 import Products from './componets/Products';
+import Home from './componets/Home';
 function App() {
  
   return (
     <>
-    
-    <Header/>
-    <Products/>
+      <Header/>
+    <Routes>
+      <Route path='/' element={ <Home/>}/>
+      <Route path='/products' element={ <Products/>}/>
+    </Routes>
       </>
   )
 }
