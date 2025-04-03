24c30b8b9af7f2a38b9680d4daef0b6f8f6f9ca8
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-03 23:27:43 +0300

    fixed a render issue

--- src/componets/product-list/ProductsList.jsx
+++ src/componets/product-list/ProductsList.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 
   export default function Products(products) {
+ 
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
