8f19d9503630f7a33b2d06a0f0938d4b47ad385b
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-03 23:26:48 +0300

    fixed post issue

--- src/api/requester.js
+++ src/api/requester.js
@@ -13,16 +13,15 @@ export default async function requester(method, url, data) {
     const responce = await fetch(url, options);
     let result = {};
     if (!responce.ok) {
-       if(responce.status!==401){
-           result = await responce.json();
-           throw result;
-       }
+        if (responce.status !== 401) {
+            result = await responce.json();
+            throw result;
+        }
     }
-    if (!url.includes("/Customer/Add")) {
-        result = await responce.json();
+    if (method !== 'POST') {
+       
+        return result = await responce.json();
     }
-
-    return result;
 
 }
 export const get = requester.bind(null, 'GET');
