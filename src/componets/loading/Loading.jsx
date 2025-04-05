f4c95bfbac0178e565b071af5ef72b410407f222
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-05 12:47:15 +0300

    removed loading order and made universal loading

--- src/componets/loading/Loading.jsx
+++ src/componets/loading/Loading.jsx
@@ -1,9 +1,9 @@
-export default function LoadingOrder() {
+export default function Loading() {
     return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <div className="text-center">
             <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mb-4"></div>
-            <p>Loading your order...</p>
+            <p>Loading...</p>
         </div>
     </div>);
 }
