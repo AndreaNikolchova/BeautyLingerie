054a8ceb5a4c3a6834db5a4d7c968ebc7f458846
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-05 18:01:22 +0300

    added loading

--- src/componets/product-list/products-all/ProductsAll.jsx
+++ src/componets/product-list/products-all/ProductsAll.jsx
@@ -1,9 +1,12 @@
 import Products from "../ProductsList"
+import Loading from "../../loading/Loading";
 import { useGetAllProducts } from "../../../hooks/useProducts";
 
 export default function ProductsAll() {
     const [products] = useGetAllProducts()
-
+if (!products || Object.keys(products).length === 0) {
+    return <Loading/>;
+  }
     return (
         <Products items={products} />
     );
--- src/componets/product-list/products-by-category/ProductsByCategory.jsx
+++ src/componets/product-list/products-by-category/ProductsByCategory.jsx
@@ -1,8 +1,12 @@
 import Products from "../ProductsList"
+import Loading from "../../loading/Loading";
 import { useGetProductsByCategory } from "../../../hooks/useProducts"
 
 export default function ProductsByCategory(props){
     const[products] = useGetProductsByCategory(props.items)
+    if (!products || Object.keys(products).length === 0) {
+        return <Loading/>;
+      }
     return(
     <Products items={products}/>
     );
