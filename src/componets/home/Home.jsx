f5e152dd87eff241b2cf1677715be9dc0d9e5243
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-05 12:49:46 +0300

    changed color bg

--- src/componets/home/Home.jsx
+++ src/componets/home/Home.jsx
@@ -20,7 +20,7 @@ const categories = [
   
   export default function Home() {
     return (
-      <div className="bg-gray-100">
+      <div className="bg-white">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
             <h2 className="text-2xl font-bold text-gray-900">Categories</h2>
